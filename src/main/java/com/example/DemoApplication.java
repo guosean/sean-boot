e5fe6077c89bfb9b01833cc45093a72bf4a02657
unknown <zhenbin.guo@QW-guozhenbin.qunarservers.com>
2017-01-10 19:26:01 +0800

    t

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -2,9 +2,26 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoApplication {
+
+
+    @RequestMapping("/")
+    String home(){
+        String str = ",boot";
+
+        String result =  "test world" + str;
+        return result;
+    }
+
+    @RequestMapping("/name")
+    String name(){
+        return "name";
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
